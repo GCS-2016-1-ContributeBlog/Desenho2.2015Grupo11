8721db77754c7f67cbbcb0928e15c8cee68be473
Karine <karine@karinepc.(none)>
2015-09-12 17:37:05 -0300

    started class user, add view e servlet controller

---
+++ ConTributeBlog/src/br/com/contributeblog/model/UserBeanModel.java
@@ -0,0 +1,33 @@
+package br.com.contributeblog.model;
+
+public class UserBeanModel {
+	int id;
+	String userName;
+	String lastName;
+	String email;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
