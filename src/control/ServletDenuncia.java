6607505bf31261cd9d40abbf7f30f6aa22436050
Karine Valença <valenca.karine@gmail.com>
2015-11-17 22:00:47 -0200

    finalizado denuncia da publicacao, utilizando fabric method

--- src/control/ServletDenuncia.java
+++ src/control/ServletDenuncia.java
@@ -10,8 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import dao.FabricaDenunciaBlogDAO;
+import dao.FabricaDenunciaPublicacaoDAO;
 import model.Blog;
 import model.DenunciaBlog;
+import model.DenunciaPublicacao;
 import model.Utilizador;
 
 @WebServlet("/ServletDenuncia")
@@ -30,7 +32,9 @@ public class ServletDenuncia extends HttpServlet{
 		String acao = request.getParameter("acao");
 		
 		DenunciaBlog denunciaBlog = new DenunciaBlog();
+		DenunciaPublicacao denunciaPublicacao = new DenunciaPublicacao();
 		FabricaDenunciaBlogDAO denunciaBlogDAO = new FabricaDenunciaBlogDAO();
+		FabricaDenunciaPublicacaoDAO denunciaPublicacaoDAO = new FabricaDenunciaPublicacaoDAO();
 		Utilizador utilizador = new Utilizador();
 		Blog blog = new Blog();
 		
@@ -51,6 +55,24 @@ public class ServletDenuncia extends HttpServlet{
 				String idBlogD= request.getParameter("idBlog");
 				request.setAttribute("idBlog", idBlogD);
 				this.rd = request.getRequestDispatcher("denunciarBlog.jsp");
+				this.rd.forward(request, response);
+				break;
+			
+		
+			case "CriarDenunciaPublicacao":
+				denunciaPublicacao.setConteudoDenuncia(request.getParameter("conteudoDenuncia"));
+				utilizador.setId(Integer.parseInt(request.getParameter("idUtilizador")));
+				int idPublicacao = Integer.parseInt(request.getParameter("idPublicacao"));
+				denunciaPublicacaoDAO.criarDenuncia(idPublicacao, denunciaPublicacao, utilizador);
+				
+				this.rd = request.getRequestDispatcher("index.jsp");
+				this.rd.forward(request, response);
+				break;
+				
+			case "InstanciaPublicacao":
+				String idPublicacaoD= request.getParameter("idPublicacao");
+				request.setAttribute("idPublicacao", idPublicacaoD);
+				this.rd = request.getRequestDispatcher("denunciarPublicacao.jsp");
 				this.rd.forward(request, response);
 				break;
 			}
