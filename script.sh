5169663459d2ce1e1e29ea825577b8eb53c21d2a
Murilo Duarte <muriloduartegoncalves@hotmail.com>
2016-06-21 21:07:56 -0300

    create initial database script

--- script.sh
+++ script.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-#Atualizando libs
-#sudo apt-get update
-
 #Rodando script de configuração do banco de dados
-#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-#sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-#sudo apt-get -y install mysql-server-5.5
+mysql -u root -proot -e "CREATE database teste;"
 
+mysql -u root -proot -e "SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;" teste
+mysql -u root -proot -e "SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;" teste
+mysql -u root -proot -e "SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';" teste
+
+mysql -u root -proot -e "CREATE  TABLE Utilizador1 (
+                                    id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                                    nome VARCHAR(50) NOT NULL,
+                                    sobrenome VARCHAR(50) NOT NULL ,
+                                    email VARCHAR(50) NOT NULL ,
+                                    genero VARCHAR(20) NOT NULL ,
+                                    senha VARCHAR(50) NOT NULL ,
+                                    apelido VARCHAR(50) NOT NULL ,
+                                    dataNascimento DATE NULL DEFAULT NULL
+                                    ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARACTER SET = utf8" teste
 
 
 
